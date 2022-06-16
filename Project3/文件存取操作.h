79ce1cb8f176f20a848fd6c3ec00a4d14c5f067b
WeiguangTWK <sunwenhao181@outlook.com>
2022-06-16 17:46:07 +0800

    修复冲突

--- Project3/文件存取操作.h
+++ Project3/文件存取操作.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #pragma once
 #define FILESTUD "studdata.dat"
 #define FILEMANA "manadata.dat"
@@ -5,4 +6,13 @@
 void save();
 void load();
 void clearall();  //É¾³ý»º´æÔÚÄÚ´æµÄ¼ÇÂ¼
+=======
+#pragma once
+#define FILESTUD "studdata.dat"
+#define FILEMANA "manadata.dat"
+
+void save();
+void load();
+void clearall();  //É¾³ý»º´æÔÚÄÚ´æµÄ¼ÇÂ¼
+>>>>>>> 84791f208b33d41f73198842be9b871011e6f59f
 void refreshall();  //Ë¢ÐÂ»º´æ
